e2958b5d77be3596458508c29b790e249a69e6af
Sung Jae Cho <rubycho@outlook.kr>
2021-04-15 10:31:30 +0900

    [VA] feat: aimybox

---
+++ example/VoiceAssistant/app/src/main/java/com/example/voiceassistant/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.voiceassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.justai.aimybox.components.AimyboxAssistantFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.assistant_container, AimyboxAssistantFragment())
+            commit()
+        }
+    }
+}
