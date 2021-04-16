60242dc3b7054ca3a02e099a3c7fc067750f1586
Sung Jae Cho <rubycho@outlook.kr>
2021-04-16 15:23:46 +0900

    [VA] feat: start with recognition mode

--- example/VoiceAssistant/app/src/main/java/com/example/voiceassistant/MainActivity.kt
+++ example/VoiceAssistant/app/src/main/java/com/example/voiceassistant/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.voiceassistant
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.justai.aimybox.Aimybox
 import com.justai.aimybox.components.AimyboxAssistantFragment
+import com.justai.aimybox.model.TextSpeech
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var aimybox: Aimybox
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +19,8 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.assistant_container, AimyboxAssistantFragment())
             commit()
         }
+
+        aimybox = (application as AimyboxApplication).aimybox
+        aimybox.speak(TextSpeech("Hi Sung jae!"), Aimybox.NextAction.RECOGNITION)
     }
 }
